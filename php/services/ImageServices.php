463d39b6344b04582cab66edbf5f1ecdf04fc5aa
Aryx <39461001+knightofnet@users.noreply.github.com>
2025-03-21 00:27:24 +0100

    Réécriture des requetes SQL en les préparant

--- php/services/ImageServices.php
+++ php/services/ImageServices.php
@@ -2,85 +2,188 @@
 
 class ImageServices
 {
-    
-    public static function getAllImages(\PDO $dbb) {
-        
+
+    /**
+     * Retourne toutes les images.
+     * @param \PDO $dbb La connexion à la BDD.
+     * @return array Un tableau contenant toutes les images.
+     */
+    public static function getAllImages(\PDO $dbb)
+    {
+
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite
+        // (en bas de page). Ceci est purement pédagogique.
+        // Pour la suite, on ne va pas préparer la requête SQL, car il n'y a pas de paramètre à passer. 
         $requeteSql = "SELECT * FROM image";
         $_SESSION['requeteSqlMemoire'][] = $requeteSql;
 
+        // On exécute la requête SQL.
         $resultat = $dbb->query($requeteSql);
-        
-        $tabloEnSortie = [];
-        while(($l=$resultat->fetch(PDO::FETCH_ASSOC))) {
-            $tabloEnSortie[] = $l;
-        }
-        
+
+        // On récupère tous les résultats de la requête dans un array/tableau PHP.
+        $tabloEnSortie = $resultat->fetchAll(PDO::FETCH_ASSOC);
+
         return $tabloEnSortie;
-        
     }
 
-    public static function getAllImagesFromUserId(\PDO $dbb, $userId) {
-        
-        $requeteSql = "SELECT * FROM image WHERE idpersonne=$userId";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
+    /**
+     * Retourne toutes les images d'un utilisateur.
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param int $userId L'identifiant de l'utilisateur.
+     * @return array Un tableau contenant toutes les images de l'utilisateur.
+     */
+    public static function getAllImagesFromUserId(\PDO $dbb, $userId)
+    {
 
-        $resultat = $dbb->query($requeteSql);
-        
-        $tabloEnSortie = [];
-        while(($l=$resultat->fetch(PDO::FETCH_ASSOC))) {
-            $tabloEnSortie[] = $l;
-        }
-        
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "SELECT * FROM image WHERE idpersonne=$userId";
+
+        // On écris la requête SQL avec un paramètre '?' pour le $userId.
+        // On parle de requête préparée car elle ne contient pas directement la valeur de $userId.
+        $requeteSql = "SELECT * FROM image WHERE idpersonne=?";
+
+        // On prépare la requête SQL.
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe le paramètre en 1ère position, avec la valeur $userId.
+        $stmt->bindValue(1, $userId, PDO::PARAM_INT);
+        // On exécute la requête.
+        $stmt->execute();
+
+        // On récupère tous les résultats de la requête dans un array/tableau PHP.
+        $tabloEnSortie = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         return $tabloEnSortie;
-        
     }
-    
-    public static function ajouteNouvelleImage(\PDO $dbb, $nomImage, $urlServeur, $userId) {
-        $requeteSql = "INSERT into image(nomImage, path, idpersonne ) VALUES ('$nomImage', '$urlServeur', $userId)";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
 
-        $dbb->query($requeteSql);
+    /**
+     * Ajoute une nouvelle image.
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param string $nomImage Le nom de l'image.
+     * @param string $urlServeur L'URL de l'image.
+     * @param int $userId L'identifiant de l'utilisateur.
+     * 
+     * @return int L'identifiant de l'image ajoutée.
+     */
+    public static function ajouteNouvelleImage(\PDO $dbb, $nomImage, $urlServeur, $userId)
+    {
 
-        // Avec $dbb->insert_id, on récupère l'identifiant de la dernière insertion, c'est-à-dire celui de l'image insérée.
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "INSERT into image(nomImage, path, idpersonne ) VALUES ('$nomImage', '$urlServeur', $userId)";
+
+        // On écrit la requête SQL avec des paramètres '?' pour les valeurs à insérer.
+        $requeteSql = "INSERT into image(nomImage, path, idpersonne ) VALUES (?, ?, ?)";
+        // On prépare la requête SQL.
+        $stmt = $dbb->prepare($requeteSql);
+
+        // On associe les paramètres avec les valeurs.
+        $stmt->bindValue(1, $nomImage, PDO::PARAM_STR);
+        $stmt->bindValue(2, $urlServeur, PDO::PARAM_STR);
+        $stmt->bindValue(3, $userId, PDO::PARAM_INT);
+
+        // On exécute la requête.
+        $stmt->execute();
+
+
+        // Avec $dbb->lastInsertId(), on récupère l'identifiant de la dernière insertion, 
+        // c'est-à-dire celui de l'image insérée.
         return $dbb->lastInsertId();
     }
 
-    public static function isExistsImageWithId(\PDO $dbb, $idImage) {
-        $requeteSql = "SELECT count(*) as C FROM image WHERE idimage = ".$idImage;
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
+    /**
+     * Vérifie si une image existe avec un identifiant donné.
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param int $idImage L'identifiant de l'image.
+     * 
+     * @return boolean Renvoie true si l'image existe, false sinon.
+     */
+    public static function isExistsImageWithId(\PDO $dbb, $idImage)
+    {
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "SELECT count(*) as C FROM image WHERE idimage = " . $idImage;
 
-        $res = $dbb->query($requeteSql);
-        
-  
-        while ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l['C'] == 1;
+        // On écrit la requête SQL avec un paramètre '?' pour le $idImage.
+        // On parle de requête préparée car elle ne contient pas directement la valeur de $idImage.
+        $requeteSql = "SELECT count(*) as C FROM image WHERE idimage = ?";
+
+        // On prépare la requête SQL.
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe le paramètre en 1ère position, avec la valeur $idImage.
+        $stmt->bindValue(1, $idImage, PDO::PARAM_INT);
+        // On exécute la requête.
+        $stmt->execute();
+
+        // On récupère LE résultat de la requête.
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne true si le résultat est égal à 1, false sinon.
+        if ($resultat != null) {
+            return $resultat['C'] == 1;
         }
-        
+
+        // Sinon, la fonction retourne false.
         return false;
     }
-        
-    public static function getImageWithId(\PDO $dbb, $idImage) {
-        $requeteSql = "SELECT nomImage, path, idpersonne FROM image WHERE idimage = ".$idImage;
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
 
-        $res = $dbb->query($requeteSql);
-        
-        if ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l;
+    /**
+     * Retourne une image avec un identifiant donné.
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param int $idImage L'identifiant de l'image.
+     * 
+     * @return array Un tableau contenant les informations de l'image.
+     */
+    public static function getImageWithId(\PDO $dbb, $idImage)
+    {
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "SELECT nomImage, path, idpersonne FROM image WHERE idimage = " . $idImage;
+
+        // On écrit la requête SQL avec un paramètre '?' pour le $idImage.
+        $requeteSql = "SELECT nomImage, path, idpersonne FROM image WHERE idimage = ?";
+        // On prépare la requête SQL.
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe le paramètre en 1ère position, avec la valeur $idImage.
+        $stmt->bindValue(1, $idImage, PDO::PARAM_INT);
+        // On exécute la requête.
+        $stmt->execute();
+
+        // On récupère LE résultat de la requête.
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne le résultat si celui-ci n'est pas null.
+        if ($resultat != null) {
+            return $resultat;
         }
 
+        // Sinon, la fonction retourne null.
         return null;
     }
 
-    public static function deleteImageAvecId(\PDO $dbb, $idImage) {
-        $requeteSql = "DELETE FROM image WHERE idimage = ".$idImage;
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
+    /**
+     * Supprime une image avec un identifiant donné.
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param int $idImage L'identifiant de l'image.
+     */
+    public static function deleteImageAvecId(\PDO $dbb, $idImage)
+    {
 
-        $dbb->query($requeteSql);
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "DELETE FROM image WHERE idimage = " . $idImage;
+
+        // On écrit la requête SQL avec un paramètre '?' pour le $idImage.
+        $requeteSql = "DELETE FROM image WHERE idimage = ?";
+        // On prépare la requête SQL.
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe le paramètre en 1ère position, avec la valeur $idImage.
+        $stmt->bindValue(1, $idImage, PDO::PARAM_INT);
+        // On exécute la requête.
+        $stmt->execute();
     }
-    
-    
 }
-
--- php/services/PersonneServices.php
+++ php/services/PersonneServices.php
@@ -3,17 +3,33 @@
 
 class PersonneServices
 {
-    public static function isThereOneUser(\PDO $dbb) {
-        $requeteSql = "SELECT count(*)  as C FROM personne";
+    /**
+     * Indique s'il y a au moins un utilisateur dans la table 'personne'
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * 
+     * @return boolean Renvoie true s'il y a au moins un utilisateur, false sinon.
+     */
+    public static function isThereOneUser(\PDO $dbb)
+    {
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        // On ne prépare pas spécialement la requête car il n'y a pas de paramètre à protéger.
+        $requeteSql = "SELECT count(*) as C FROM personne";
         $_SESSION['requeteSqlMemoire'][] = $requeteSql;
 
-        $res = $dbb->query($requeteSql);
-        
-        while ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l['C'] > 0;
+        // On exécute la requête SQL.
+        $stmt = $dbb->query($requeteSql);
+
+        // On récupère le résultat de la requête.
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne true si le résultat est égal à 1, false sinon.
+        if ($resultat != null) {
+            return $resultat['C'] > 0;
         }
-        
+
+        // Sinon, la fonction retourne false.
         return false;
     }
 
@@ -26,75 +42,154 @@ class PersonneServices
      * 
      * @return boolean Renvoie true si l'utilisateur existe, false sinon.
      */
-    public static function isUserExists(\PDO $dbb, $nom) {
-        $requeteSql = "SELECT count(*)  as C FROM personne WHERE nompersonne='$nom'";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
+    public static function isUserExists(\PDO $dbb, $nom)
+    {
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.       
+        $_SESSION['requeteSqlMemoire'][] = "SELECT count(*)  as C FROM personne WHERE nompersonne='$nom'";
 
-        $res = $dbb->query($requeteSql);
-        
-        while ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l['C'] == 1;
+        // On écrit la requête SQL avec des paramètres '?' pour les valeurs à insérer
+        $requeteSql = "SELECT count(*)  as C FROM personne WHERE nompersonne= ?";
+        // On prépare la requête SQL
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe les paramètres avec les valeurs
+        $stmt->bindValue(1, $nom, PDO::PARAM_STR);
+        // On exécute la requête
+        $stmt->execute();
+
+        // On récupère LE résultat de la requête
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne le résultat si celui-ci n'est pas null
+        if ($resultat != null) {
+            return $resultat['C'] == 1;
         }
-        
+
+        // Sinon, la fonction retourne false
+
+
+    }
+
+    public static function isUserExistsWithPassword(\PDO $dbb, $nom, $passwd)
+    {
+
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "SELECT count(*) as C FROM personne WHERE nompersonne='$nom' AND passwd='$passwd'";
+
+        // On écrit la requête SQL avec des paramètres '?' pour les valeurs à insérer
+        $requeteSql = "SELECT count(*) as C FROM personne WHERE nompersonne= ? AND passwd= ?";
+        // On prépare la requête SQL
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe les paramètres avec les valeurs
+        $stmt->bindValue(1, $nom, PDO::PARAM_STR);
+        $stmt->bindValue(2, $passwd, PDO::PARAM_STR);
+        // On exécute la requête
+        $stmt->execute();
+
+        // On récupère LE résultat de la requête
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne le résultat si celui-ci n'est pas null
+        if ($resultat != null) {
+            return $resultat['C'] == 1;
+        }
+
+        // Sinon, la fonction retourne false
         return false;
     }
-    
-    public static function isUserExistsWithPassword(\PDO $dbb, $nom, $passwd) {
-        
-        $requeteSql = "SELECT count(*) as C FROM personne WHERE nompersonne='$nom' AND passwd='$passwd'";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
-        
-        $res = $dbb->query($requeteSql );
-        
-        while ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l['C'] == 1;
+
+    /**
+     * Retourne l'identifiant de l'utilisateur transmis dans la variable $nom
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param string $nom Le nom de l'utilisateur.
+     * @param string $passwd Le mot de passe de l'utilisateur.
+     * 
+     * @return int L'identifiant de l'utilisateur, ou -1 si l'utilisateur n'existe pas.
+     */
+    public static function getUserId(\PDO $dbb, $nom, $passwd)
+    {
+
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "SELECT idpersonne FROM personne WHERE nompersonne='$nom' AND passwd='$passwd'";
+
+        // On écrit la requête SQL avec des paramètres '?' pour les valeurs à insérer
+        $requeteSql = "SELECT idpersonne FROM personne WHERE nompersonne= ? AND passwd=?";
+        // On prépare la requête SQL
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe les paramètres avec les valeurs
+        $stmt->bindValue(1, $nom, PDO::PARAM_STR);
+        $stmt->bindValue(2, $passwd, PDO::PARAM_STR);
+        // On exécute la requête
+        $stmt->execute();
+
+        // On récupère LE résultat de la requête
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne le résultat si celui-ci n'est pas null
+        if ($resultat != null) {
+            return $resultat['idpersonne'];
         }
-        
-        return false;
-        
-    }
-    
-    public static function getUserId(\PDO $dbb, $nom, $passwd) {
-        
-        $requeteSql = "SELECT idpersonne FROM personne WHERE nompersonne='$nom' AND passwd='$passwd'";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
-        
-        $res = $dbb->query($requeteSql );
-        
-        while ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l['idpersonne'];
-        }
-        
+
+        // Sinon, la fonction retourne -1
         return -1;
-        
     }
 
-    public static function getUserById(\PDO $dbb, $idPersonne) {
-        
-        $requeteSql = "SELECT nompersonne FROM personne WHERE idpersonne=$idPersonne";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
-        
-        $res = $dbb->query($requeteSql );
-        
-        while ( ($l=$res->fetch(PDO::FETCH_ASSOC)) != null) {
-            
-            return $l['nompersonne'];
+    public static function getUserById(\PDO $dbb, $idPersonne)
+    {
+
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite 
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "SELECT nompersonne FROM personne WHERE idpersonne=$idPersonne";
+
+        $requeteSql = "SELECT nompersonne FROM personne WHERE idpersonne = ?";
+        // On prépare la requête SQL
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe les paramètres avec les valeurs
+        $stmt->bindValue(1, $idPersonne, PDO::PARAM_INT);
+        // On exécute la requête
+        $stmt->execute();
+
+        // On récupère LE résultat de la requête
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // La fonction retourne le résultat si celui-ci n'est pas null
+        if ($resultat != null) {
+            return $resultat['nompersonne'];
         }
-        
+
+        // Sinon, la fonction retourne null
         return null;
-        
     }
 
-    public static function addNewPersonne(\PDO $dbb, $nom, $passwd) {
-        $requeteSql = "INSERT personne(nompersonne, passwd) VALUES ('$nom', '$passwd')";
-        $_SESSION['requeteSqlMemoire'][] = $requeteSql;
+    /**
+     * Ajoute un nouvel utilisateur dans la table 'personne'
+     * 
+     * @param \PDO $dbb La connexion à la BDD.
+     * @param string $nom Le nom de l'utilisateur.
+     * @param string $passwd Le mot de passe de l'utilisateur.
+     * 
+     * @return int L'identifiant de l'utilisateur ajouté.
+     */
+    public static function addNewPersonne(\PDO $dbb, $nom, $passwd)
+    {
+        // On enregistre la requête en mémoire (dans la session) pour pouvoir l'afficher ensuite
+        // (en bas de page). Ceci est purement pédagogique.
+        $_SESSION['requeteSqlMemoire'][] = "INSERT personne(nompersonne, passwd) VALUES ('$nom', '$passwd')";
 
-        $dbb->query($requeteSql);
+        // On écrit la requête SQL avec des paramètres '?' pour les valeurs à insérer
+        $requeteSql = "INSERT personne(nompersonne, passwd) VALUES (?,?)";
+        // On prépare la requête SQL
+        $stmt = $dbb->prepare($requeteSql);
+        // On associe les paramètres avec les valeurs
+        $stmt->bindValue(1, $nom, PDO::PARAM_STR);
+        $stmt->bindValue(2, $passwd, PDO::PARAM_STR);
+        // On exécute la requête
+        $stmt->execute();
 
+        // On retourne l'identifiant de l'utilisateur ajouté
         return $dbb->lastInsertId();
     }
-    
 }
